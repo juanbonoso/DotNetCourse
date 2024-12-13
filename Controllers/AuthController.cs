4b7476be5d12467bf74ab2bad1e938b6f454aee0
juanbonoso <juanbonoso@gmail.com>
2024-12-13 17:08:22 -0500

    Use Jwt Tokens and helpers to authenticate the user

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,25 +1,35 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Security.Cryptography;
 using System.Text;
 using DotNetApi.Data;
 using DotNetApi.Dtos;
+using DotNetApi.Helpers;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Cryptography.KeyDerivation;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
+using Microsoft.IdentityModel.Tokens;
 
 namespace DotNetApi.Controllers
 {
+    [Authorize]
     [ApiController]
+    [Route("[controller]")]
     public class AuthController : ControllerBase
     {
         private readonly DataContextDapper _dapper;
         private readonly IConfiguration _config;
+        private readonly AuthHelper _authHelper;
 
         public AuthController(IConfiguration config)
         {
             _config = config;
             _dapper = new DataContextDapper(_config);
+            _authHelper = new AuthHelper(_config);
         }
 
+        [AllowAnonymous]
         [HttpPost("Register")]
         public IActionResult Register(UserForRegistrationDto userForRegistrationDto)
         {
@@ -41,10 +51,10 @@ namespace DotNetApi.Controllers
                 rng.GetNonZeroBytes(passwordSalt);
             }
 
-            byte[] passwordHash = GetPasswordHash(userForRegistrationDto.Password, passwordSalt);
+            byte[] passwordHash = _authHelper.GetPasswordHash(userForRegistrationDto.Password, passwordSalt);
 
             string sqlAddAuth = "INSERT INTO TutorialAppSchema.Auth (Email, PasswordHash, PasswordSalt) VALUES (@Email, @PasswordHash, @PasswordSalt)";
-            var parameters = new 
+            var parameters = new
             {
                 Email = userForRegistrationDto.Email,
                 PasswordHash = passwordHash,
@@ -57,9 +67,28 @@ namespace DotNetApi.Controllers
                 return StatusCode(500, "Internal server error");
             }
 
+            string sqlAddUser = @"
+                INSERT INTO TutorialAppSchema.Users (
+                    [FirstName],
+                    [LastName],
+                    [Email],
+                    [Gender],
+                    [Active]
+                ) VALUES (
+                    '" + userForRegistrationDto.FirstName + @"',
+                    '" + userForRegistrationDto.LastName + @"',
+                    '" + userForRegistrationDto.Email + @"',
+                    '" + userForRegistrationDto.Gender + @"',
+                    " + 1 + ")";
+            if (!_dapper.ExecuteSql(sqlAddUser))
+            {
+                return StatusCode(500, "Failed to add user");
+            }
+
             return Ok();
         }
 
+        [AllowAnonymous]
         [HttpPost("Login")]
         public IActionResult Login(UserForLoginDto userForLoginDto)
         {
@@ -72,30 +101,40 @@ namespace DotNetApi.Controllers
                 return Unauthorized();
             }
 
-            byte[] passwordHash = GetPasswordHash(userForLoginDto.Password, userForConfirmationDto.PasswordSalt);
+            byte[] passwordHash = _authHelper.GetPasswordHash(userForLoginDto.Password, userForConfirmationDto.PasswordSalt);
 
             if (!passwordHash.SequenceEqual(userForConfirmationDto.PasswordHash))
             {
                 return StatusCode(500, "Incorrect Password");
             }
 
-            return Ok("Login successful");
+            string sqlForUserId = "SELECT UserId FROM TutorialAppSchema.Users WHERE Email = @Email";
+            int userId = _dapper.LoadDataSingleWithParams<int>(sqlForUserId, new { userForLoginDto.Email });
+
+
+
+            return Ok(new Dictionary<string, string>
+            {
+                { "ok", "true" },
+                {"message", "User logged in successfully"},
+                { "token", _authHelper.CreateToken(userId) }
+            });
         }
 
-        private byte[] GetPasswordHash(string password, byte[] passwordSalt)
+
+        [HttpGet("RefreshToken")]
+        public IActionResult RefreshToken()
         {
-            string passwordSaltPlusString = _config.GetSection("AppSettings:PasswordKey").Value +
-                        Convert.ToBase64String(passwordSalt);
+            string userId = User.FindFirst("userId")?.Value + "";
+            string userIdSQL = "SELECT UserId FROM TutorialAppSchema.Users WHERE UserId = @UserId";
+            int userIdFromDB = _dapper.LoadDataSingleWithParams<int>(userIdSQL, new { userId });
 
-            return KeyDerivation.Pbkdf2(
-                password,
-                salt: Encoding.ASCII.GetBytes(passwordSaltPlusString),
-                prf: KeyDerivationPrf.HMACSHA256,
-                iterationCount: 10000,
-                numBytesRequested: 256 / 8
-            );
-
+            return Ok(new Dictionary<string, string>
+            {
+                { "ok", "true" },
+                {"message", "Token refreshed successfully"},
+                { "token", _authHelper.CreateToken(userIdFromDB) }
+            });
         }
-
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
+using System.Text;
 using DotNetApi.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -30,6 +33,23 @@ builder.Services.AddCors((options) =>
     });
 
 builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+string? tokenKeyString = builder.Configuration.GetSection("AppSettings:TokenKey").Value;
+SymmetricSecurityKey tokenKey = new SymmetricSecurityKey(
+    Encoding.UTF8.GetBytes(tokenKeyString ?? ""));
+
+TokenValidationParameters tokenValidationParameters = new TokenValidationParameters
+{
+    IssuerSigningKey = tokenKey,
+    ValidateIssuer = false,
+    ValidateIssuerSigningKey = false,
+    ValidateAudience = false
+};
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = tokenValidationParameters;
+    });
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -45,6 +65,9 @@ else
     app.UseCors("ProdCors");
     app.UseHttpsRedirection();
 }
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
